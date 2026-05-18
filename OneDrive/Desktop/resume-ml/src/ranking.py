a59e7dc2366d661e002bd68a206abbe9399f1497
LAKSHMI ABHISHIKTHA <abhishikthasouram@gmail.com>
2026-05-18 14:35:08 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/resume-ml/src/ranking.py
+++
@@ -1,8 +0,0 @@
-def rank_jobs(df):
-    
-    ranked_df = df.sort_values(
-        by='similarity_score',
-        ascending=False
-    )
-
-    return ranked_df
--- OneDrive/Desktop/support-ticket-classification/src/predict.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-model = joblib.load('../models/ticket_classifier.pkl')
-
-ticket = ["Payment failed but amount deducted"]
-
-prediction = model.predict(ticket)
-
-print(prediction)
